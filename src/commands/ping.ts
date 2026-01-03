e2608e7d4f788e0931b42a97635947b931c8faa2
AlfrenD <45785448+AlfrenD@users.noreply.github.com>
2026-01-03 14:13:16 +0300

    Version 0.0.3 HOTFIX this means my bot have a command which working

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -9,7 +9,6 @@ export default {
   async execute(interaction: CommandInteraction) {
     const sent = await interaction.reply({
       content: "Пидарас ты хуесосина соси хуй пизда ты старый хуй да да я",
-      flags: MessageFlags.Ephemeral,
       withResponse: true,
     });
     console.log(sent.resource?.message);
--- src/start-bot.ts
+++ src/start-bot.ts
@@ -7,54 +7,41 @@ import {
   Routes,
 } from "discord.js";
 
-import fs from "fs";
-import path, { dirname } from "path";
 //test import { ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js";
 import { Client } from "./client";
 import { constants } from "@/config/constants";
-import { fileURLToPath } from "url";
 
 // https://github.com/ZyhlohYT/BasicYTDiscordBot/blob/main/index.js
 // https://www.youtube.com/watch?v=Dgy3EJ3HtMw
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const deployCommands = async () => {
-  try {
-    const arrCommands = [];
-
-    const commandFiles = fs
-      .readdirSync(path.join(__dirname, "commands"))
-      .filter((file) => file.endsWith(".ts"));
-
-    for (const file of commandFiles) {
-      const command = await import(`./commands/${file}`);
-      if ("data" in command && "execute" in command) {
-        arrCommands.push(command);
-      } else {
-        console.log(
-          `WARNONG: The command ${file} is missing a required 'data' or 'execute'`,
-        );
-      }
-    }
-    const rest = new REST().setToken(constants.client.token); //TODO прочитай про discord REST
-
-    console.log(`Started refreshing application slash commands globally.`);
-
-    const data = await rest.put(
-      Routes.applicationCommands(constants.client.id),
-      { body: arrCommands },
-    );
-
-    console.log(`Successfully reloaded all commands`);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
 const client = new Client();
 
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.log(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
 // test button
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
